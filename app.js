cd5ada8c80c4ccdbca5766858e905c9ea0d3960d
Wyatt Fry <wyattfry@gmail.com>
2018-01-31 15:56:20 -0800

    Almost done with interaction

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 // TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {acquired: false};
+  }
+  onListItemClick() {
+    this.setState({acquired: !this.state.acquired});
+    console.log('mouse over', this);
+  }
+  render() {
+    var style = {
+      font-weight: this.state.acquired ? 'bold' : 'normal'
+    };
+    
+    return (<li style={style} onClick={this.onListItemClick.bind(this)}>
+      {this.props.item}</li>);
+  }
+} 
+
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map( item => <GroceryListItem item={item} />)}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList items={['Apples', 'Bananas', 'Kiwis', 'Oranges']} />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
